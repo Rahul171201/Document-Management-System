57615710854b9830c994f60d6528ebaf60dd7fe3
Rahul Roy <70642284+Rahul171201@users.noreply.github.com>
2020-12-04 23:56:03 +0530

    Added Tag class

---
+++ Tag.java
@@ -0,0 +1,11 @@
+public class Tag {
+    // public static String[][] StringList = new String[100][100];
+    // public static int[] no_tags = new int[100];
+
+    public static String[] tags = new String[100];
+
+    public Tag() {
+
+    }
+
+}
